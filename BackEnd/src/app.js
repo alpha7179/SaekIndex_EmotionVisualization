8e577336af2698d5ae31f6f13facae3cc0d91e7b
alpha7179 <alpha_loading7179@naver.com>
2025-10-05 16:07:39 +0900

    수정

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 function createApp() {
   const app = express();
 
-  app.use(cors());
+  app.use(cors({ origin: '*' }));
   app.use(express.json());
 
   app.get('/health', (req, res) => {
